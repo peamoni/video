d54f5cdbb5c70765e4ed1c87a9bfcd410a96fd4f
Vivien <vivien.haese@gmail.com>
2022-06-04 21:25:45 +0200

    Add gif support

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import { CallableContext } from "firebase-functions/lib/providers/https";
 import fetch from "node-fetch";
+import UnZip = require("node-zip");
 
 // The Firebase Admin SDK to access Firestore.
 import * as admin from "firebase-admin";
@@ -36,29 +37,24 @@ export const render = functions
     const callbackURL = `${process.env.CALLBACK_URL}?uid=${writeResult.id}`;
 
     // HTTP Post to GitHub Actions
-    const response = await fetch(process.env.GITHUB_ACTION_URL!, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.GITHUB_ACTIONS_API_BEARER}`,
-      },
-      body: JSON.stringify({
-        inputs: {
-          callback_url: callbackURL,
-          ...data.conf,
+    const response = await fetch(
+      `${process.env.GITHUB_ACTION_URL!}/workflows/render-video.yml/dispatches`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.GITHUB_ACTIONS_API_BEARER}`,
         },
-        ref: process.env.GITHUB_ACTION_BRANCH,
-      }),
-    });
-    console.log(
-      JSON.stringify({
-        inputs: {
-          callback_url: callbackURL,
-          ...data.conf,
-        },
-        ref: process.env.GITHUB_ACTION_BRANCH,
-      })
+        body: JSON.stringify({
+          inputs: {
+            callback_url: callbackURL,
+            ...data.conf,
+          },
+          ref: process.env.GITHUB_ACTION_BRANCH,
+        }),
+      }
     );
+
     await response.text();
     if (response.status !== 200) {
       return { message: "Oups...", code: response.status };
@@ -85,10 +81,71 @@ export const update = functions
         resp.status(404).send("Not found");
         return;
       }
-      await admin.firestore().collection("renders").doc(uid).update({
+
+      const runData = {
         status,
         runId,
-      });
+        artifact: null,
+      };
+      if (status === RunStatus.Created) {
+        const response = await fetch(
+          `${process.env.GITHUB_ACTION_URL!}/runs/${runId}/artifacts`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.GITHUB_ACTIONS_API_BEARER}`,
+            },
+          }
+        );
+        const artifacts: { total_count: number; artifacts: any[] } =
+          await response.json();
+        if (artifacts.total_count === 1) {
+          runData.artifact = artifacts.artifacts[0];
+        }
+      }
+      await admin.firestore().collection("renders").doc(uid).update(runData);
       resp.status(200).send("OK");
+    }
+  );
+
+export const video = functions
+  .runWith({ secrets: ["GITHUB_ACTIONS_API_BEARER"] })
+  .region("europe-west1")
+  .https.onRequest(
+    async (req: functions.https.Request, resp: functions.Response<any>) => {
+      const uid = req.query.uid as string;
+
+      if (!uid) {
+        resp.status(400).send("Bad request");
+        return;
+      }
+      const run = await admin.firestore().collection("renders").doc(uid).get();
+      if (!run.exists) {
+        resp.status(404).send("Run Not found");
+        return;
+      }
+      if (!run.data()!.artifact) {
+        resp.status(404).send("Artifact Not found");
+        return;
+      }
+
+      const response = await fetch(run.data()!.artifact.archive_download_url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_ACTIONS_API_BEARER}`,
+        },
+      });
+      const data = await response.buffer();
+      // unzip data from buffer
+      const zip = new UnZip();
+      zip.load(data);
+      const video = zip.files["video.mp4"];
+      if (!video) {
+        resp.status(404).send("Video Not found");
+        return;
+      }
+      resp.set("Content-Type", "video/mp4");
+      resp.status(200).send(video.asNodeBuffer());
     }
   );
--- src/remotion/MyComp.tsx
+++ src/remotion/MyComp.tsx
@@ -1,4 +1,9 @@
+import { Gif } from "@remotion/gif";
+import { useVideoConfig } from "remotion";
+
 export const MyComp: React.FC<{ ticker: string }> = ({ ticker }) => {
+  const { width, height } = useVideoConfig();
+
   return (
     <div
       style={{
@@ -7,6 +12,14 @@ export const MyComp: React.FC<{ ticker: string }> = ({ ticker }) => {
       }}
     >
       Hello {ticker}!
+      <div>
+        <Gif
+          src="https://media.giphy.com/media/3o72F7YT6s0EMFI0Za/giphy.gif"
+          width={width}
+          height={height}
+          fit="fill"
+        />
+      </div>
     </div>
   );
 };
